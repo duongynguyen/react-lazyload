3c94825d6f008e7f275b034bf93124bb751f4962
nguyenduong <duongynguyen0501@gmail.com>
2019-05-29 15:52:27 +0700

    Demo with react-lazyload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data";
+import LazyLoad from "react-lazyload";
 
-function App() {
+const Spinner = () => (
+  <div className="post loading">
+    <h5>Loading...</h5>
+  </div>
+);
+
+const Post = obj => (
+  <div className="post">
+    <LazyLoad
+      once={true}
+      placeholder={
+        <img src={`https://picsum.photos/id/${obj.id}/5/5`} alt="..." />
+      }
+    >
+      <div className="post-img">
+        <img src={`https://picsum.photos/id/${obj.id}/200/200`} alt="..." />
+      </div>
+    </LazyLoad>
+    <div className="post-body">
+      <h4>{obj.title}</h4>
+      <p>{obj.body}</p>
+    </div>
+  </div>
+);
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>LazyLoad Demo</h2>
+      <div className="post-container">
+        {data.map(post => (
+          <LazyLoad
+            key={post.id}
+            height={100}
+            offset={[-100, 100]}
+            placeholder={<Spinner />}
+          >
+            <Post key={post.id} {...post} />
+          </LazyLoad>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
